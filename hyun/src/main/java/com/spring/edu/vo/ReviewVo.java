8a3b238d667f47a69b1b5b46fdc84af9b55f3cda
UJ <luj84@naver.com>
2018-05-21 23:34:29 +0900

    REVIEW게시판 VO객체 생성->lombok을 사용으로 멤버변수만 선언, get/set은 자동생성

--- hyun/src/main/java/com/spring/edu/vo/ReviewVo.java
+++ hyun/src/main/java/com/spring/edu/vo/ReviewVo.java
@@ -1,8 +1,32 @@
 package com.spring.edu.vo;
 
+import java.util.Date;
 import lombok.Data;
 
 @Data
 public class ReviewVo {
-
+	
+	/*게시판 번호*/
+	private int br_no;
+	
+	/*게시판 아이디*/
+	private String ur_id;
+	
+	/*게시판 제목*/
+	private String br_title;
+	
+	/*게시판 내용*/
+	private String br_content;
+	
+	/*게시판 날짜*/
+	private Date br_date;
+	
+	/*게시판 조회수*/
+	private int br_hits;
+	
+	/*게시판 부모번호(답글을 위한)*/
+	private int br_prno;
+	
+	/*게시판 삭제여부*/
+	private char br_en;
 }
