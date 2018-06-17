95a56ed6627b6c581ce22e223578f989da82c8a3
UJ <luj84@naver.com>
2018-06-17 20:04:58 +0900

    review게시판 조회수 완료

--- hyun/src/main/java/com/spring/edu/service/Impl/ReviewServiceImpl.java
+++ hyun/src/main/java/com/spring/edu/service/Impl/ReviewServiceImpl.java
@@ -2,51 +2,54 @@ package com.spring.edu.service.Impl;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.edu.dao.ReviewDao;
 import com.spring.edu.service.ReviewService;
+import com.spring.edu.vo.AnswerBoardVo;
 import com.spring.edu.vo.BoardCriteria;
 import com.spring.edu.vo.ReviewVo;
 import com.spring.edu.vo.SearchCriteria;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
-	
-	@Autowired
-	private ReviewDao dao;
 
-	@Override
-	public void regist(ReviewVo vo) throws Exception {
-		dao.create(vo);
-	}
+    @Autowired
+    private ReviewDao dao;
 
-	@Override
-	public ReviewVo read(int brNo) throws Exception {
-		return dao.read(brNo);
-	}
+    @Override
+    public void regist(ReviewVo vo) throws Exception {
+        dao.create(vo);
+    }
 
-	@Override
-	public void update(ReviewVo vo) throws Exception {
-		dao.update(vo);
-	}
+    @Override
+    public ReviewVo read(int brNo) throws Exception {
+        return dao.read(brNo);
+    }
 
-	@Override
-	public void delete(int brNo) throws Exception {
-		dao.delete(brNo);
-	}	
-	
-	@Override
-	public List<ReviewVo> listPaging(BoardCriteria cri) throws Exception {
-		return dao.listPaging(cri);
-	}
+    @Override
+    public void update(ReviewVo vo) throws Exception {
+        dao.update(vo);
+    }
 
-	@Override
-	public int countPaging(BoardCriteria cri) throws Exception {
-		return dao.countPaging(cri);
-	}
+    @Override
+    public void delete(int brNo) throws Exception {
+        dao.delete(brNo);
+    }
+
+    @Override
+    public List<ReviewVo> listPaging(BoardCriteria cri) throws Exception {
+        return dao.listPaging(cri);
+    }  
+
+    @Override
+    public int countPaging(BoardCriteria cri) throws Exception {
+        return dao.countPaging(cri);
+    }
 
     @Override
     public List<ReviewVo> listSearch(SearchCriteria cri) throws Exception {
@@ -57,11 +60,11 @@ public class ReviewServiceImpl implements ReviewService {
     public int listSearchCount(SearchCriteria cri) throws Exception {
         return dao.listSearchCount(cri);
     }
-	
-	
 
-	
-	
+    @Override
+    public void viewCnt(int brNo, HttpSession session) throws Exception {       
+        dao.viewCnt(brNo);
+    };
 
 
 }
