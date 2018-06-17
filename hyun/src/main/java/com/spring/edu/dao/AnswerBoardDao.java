d1c6265d55f584e8cce9c1ec511292094395a791
UJ <luj84@naver.com>
2018-06-17 20:06:06 +0900

    Rest 방식의 댓글처리 테스트용 작성

---
+++ hyun/src/main/java/com/spring/edu/dao/AnswerBoardDao.java
@@ -0,0 +1,28 @@
+package com.spring.edu.dao;
+
+import java.util.List;
+
+import com.spring.edu.vo.AnswerBoardVo;
+import com.spring.edu.vo.BoardCriteria;
+
+public interface AnswerBoardDao {
+
+    /*페이지 번호에 의한 댓글 리스트*/
+    public List<AnswerBoardVo> listPaging(int brNo, BoardCriteria cri) throws Exception;
+    
+    public List<AnswerBoardVo> list(int brNo) throws Exception;
+    
+    /*댓글 생성*/
+    public void create(AnswerBoardVo vo) throws Exception;
+    
+    /*댓글 수정*/
+    public void update(AnswerBoardVo vo) throws Exception;
+    
+    /*댓글 삭제*/
+    public void delete(int asNo) throws Exception;
+    
+    /*댓글 전체갯수*/
+    public int count(int brNo) throws Exception;
+    
+    
+}
