8255e2bcfabc32b2942e54bb569b47062b8c8bad
UJ <luj84@naver.com>
2018-08-17 14:13:50 +0900

    상품리스트 메인화면

--- hyun/src/main/java/com/spring/edu/controller/MainController.java
+++ hyun/src/main/java/com/spring/edu/controller/MainController.java
@@ -1,15 +1,34 @@
 package com.spring.edu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.edu.service.ProductService;
+import com.spring.edu.utill.Criteria;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+	private ProductService service;
 
+	/**
+	  * @Method Name : Main
+	  * @작성일 : 2018. 7. 2.
+	  * @작성자 : 유현재
+	  * @변경이력 : 
+	  * @Method 설명 : 메인페이지
+	  * @param cri
+	  * @param modelAndView
+	  * @return
+	  */
 	@RequestMapping("/")
-	public ModelAndView boardMain(ModelAndView modelAndView) {
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>메인페이지접속");
+	public ModelAndView Main(@ModelAttribute("cri")Criteria cri, ModelAndView modelAndView) {
+		cri.setPerPageNum(8);
+		modelAndView.addObject("productList", service.shopList(cri));
 		modelAndView.setViewName("/home");
 		return modelAndView;
 	}
